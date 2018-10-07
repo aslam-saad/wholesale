f94a7499b5c6498b9ab46975c7794c6f6ad1e47d
Eslam Ghazal <eslamm.gazal@gmail.com>
2018-10-07 12:12:43 +0200

    Update sale.R

--- sale.R
+++ sale.R
@@ -14,7 +14,6 @@ sale.data$Channel <- fct_recode(sale.data$Channel, Horeca = "1", Retail = "2")
 
 sale.data$Region <- as.factor(sale.data$Region)
 sale.data$Region <- fct_recode(sale.data$Region, Lisbon = "1", Oporto = "2", Other = "3")
-sale.data$Region <- NULL
 
 ##Split to train and test sets
 set.seed(75)
